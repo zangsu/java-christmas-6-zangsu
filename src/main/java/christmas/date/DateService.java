de3cf4b7cf467c5c65e19c3b2b84ddddd1911771
zangsu <zangsu_@naver.com>
2023-11-11 12:55:20 +0900

    feat : DateService 생성

---
+++ src/main/java/christmas/date/DateService.java
@@ -0,0 +1,18 @@
+package christmas.date;
+
+import christmas.date.model.PromotionDay;
+
+/**
+ * 날짜와 관련된 책임을 처리하는 클래스
+ */
+public class DateService {
+
+    /**
+     * 정적 팩토리 메서드인 Promotion.from()을 통해 {@link PromotionDay}를 생성 후 반환
+     * @param date - 생성할 날짜 정보
+     * @return 생성된 {@link PromotionDay}
+     */
+    public PromotionDay getPromotionDay(int date){
+        return PromotionDay.from(date);
+    }
+}
