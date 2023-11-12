4793d1f5c967e2894c83aaea6c55cc42dff70e8f
zangsu <zangsu_@naver.com>
2023-11-13 01:18:35 +0900

    feat : InputView 구현

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,20 @@
+package christmas.view;
+
+import christmas.view.io.Printer;
+import christmas.view.io.Reader;
+import java.util.List;
+
+public class InputView {
+    Printer printer = new Printer();
+    Reader reader = new Reader();
+
+    public int getVisitDate(){
+        printer.printMessage(ViewConst.DATE_REQUEST_MESSAGE);
+        return reader.getInteger();
+    }
+
+    public List<String> getOrders(){
+        printer.printMessage(ViewConst.ORDER_REQUEST_MESSAGE);
+        return reader.getStringsByDelimiter(ViewConst.ORDERS_DELIMITER);
+    }
+}
---
+++ src/main/java/christmas/view/ViewConst.java
@@ -0,0 +1,9 @@
+package christmas.view;
+
+public interface ViewConst {
+
+    String ORDERS_DELIMITER = ",";
+
+    String DATE_REQUEST_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    String ORDER_REQUEST_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+}
